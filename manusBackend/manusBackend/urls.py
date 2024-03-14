74e08f230cdbd5637b00cf4f971bc645aaf314d3
VictorBriganti <jvbriganti@gmail.com>
2024-03-14 17:15:42 -0300

    [Views][Templates] Adicionado primeira view
    Adicionado rota para o root do site

--- manusBackend/manusBackend/urls.py
+++ manusBackend/manusBackend/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from usuario.views import my_view
+from django.urls import include, path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/',my_view),
+    path('',include("usuario.urls")),
 ]
