de73cf9c30764070dbdecdf8968f835cfe62e835
MattSaito <matheussaitofs@gmail.com>
2024-03-14 16:27:08 -0300

    [view] [url] criei uma view que retorna uma
    resposta, importei esta view para a url e criei o
    path /user

--- manusBackend/manusBackend/urls.py
+++ manusBackend/manusBackend/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from usuario.views import my_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/',my_view),
 ]
