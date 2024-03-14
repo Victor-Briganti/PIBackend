42ca80f1f994e4800fa544b3de173c8eb3b4b0ab
VictorBriganti <jvbriganti@gmail.com>
2024-03-14 18:14:54 -0300

    [View][URL] Adicionado links rotas iniciais
    Adicionado rota para a pagina "Sobre Nós" e "Contato".

--- manusBackend/usuario/urls.py
+++ manusBackend/usuario/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("sobre/", views.sobre, name="sobre"),
+    path("contato/", views.contato, name="contato"),
 ]
