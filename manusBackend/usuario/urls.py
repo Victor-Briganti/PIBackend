d66a269a71c8056564e0a63830a1fc30aab76034
Victor Briganti <jvbriganti@gmail.com>
2024-03-16 00:57:07 -0300

    [View][Model] Adicionado views para cadastro e login

--- manusBackend/usuario/urls.py
+++ manusBackend/usuario/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("sobre/", views.sobre, name="sobre"),
     path("contato/", views.contato, name="contato"),
+    path("login/", views.login_page, name="login"),
+    path("cadastro/", views.cadastro, name="cadastro"),
+    # TODO: Excluir a página de sucesso
+    path("sucesso/", views.sucesso, name="sucesso"),
 ]
