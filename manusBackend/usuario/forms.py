7ebaf215b360247ab35d69abc42206f12145e51b
Victor Briganti <jvbriganti@gmail.com>
2024-03-16 11:28:34 -0300

    [View][Template] Removido conteúdo estático
    Aplicação irá se utilizar de API's para conversar com o front.
    Não há mais necessidade de templates e views para o usuário.

--- manusBackend/usuario/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Usuario
-
-
-class UsuarioForm(forms.ModelForm):
-    class Meta:
-        model = Usuario
-        fields = ["cpf", "nome", "senha", "email", "telefone"]
--- manusBackend/usuario/urls.py
+++ manusBackend/usuario/urls.py
@@ -2,12 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("sobre/", views.sobre, name="sobre"),
-    path("contato/", views.contato, name="contato"),
-    path("login/", views.login_page, name="login"),
-    path("cadastro/", views.cadastro, name="cadastro"),
-    # TODO: Excluir a página de sucesso
-    path("sucesso/", views.sucesso, name="sucesso"),
-]
+urlpatterns = []
