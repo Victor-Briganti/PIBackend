296cfcf2e07dccc11e9981faa7c7645d79207ba9
Victor Briganti <jvbriganti@gmail.com>
2024-03-18 13:45:55 -0300

    [Models] Adicionado modelo de Usuário

--- adopet_backend/usuario/models.py
+++ adopet_backend/usuario/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+    """
+    Classe que representa um usuário do sistema
+
+    Todos usuários do sistema devem ser cadastrados nessa classe.
+    Somente usuários administradores podem cadastrar novos animais e realizar postagens.
+    """
+
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    email = models.EmailField()
+    senha = models.CharField(max_length=50)
+    administrador = models.BooleanField(default=False)
