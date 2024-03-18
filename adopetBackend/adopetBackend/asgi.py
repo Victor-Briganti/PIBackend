496c599aa63f1405f3a94f7665391e5c190f1cb2
Victor Briganti <jvbriganti@gmail.com>
2024-03-18 00:26:23 -0300

    Alterado nome do projeto de manusBackend para adopetBackend

--- adopetBackend/adopetBackend/asgi.py
+++ adopetBackend/adopetBackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for manusBackend project.
+ASGI config for adopetBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manusBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adopetBackend.settings')
 
 application = get_asgi_application()
--- adopetBackend/adopetBackend/urls.py
+++ adopetBackend/adopetBackend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for manusBackend project.
+URL configuration for adopetBackend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
