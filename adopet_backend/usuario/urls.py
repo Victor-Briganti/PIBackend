ba96773a2e456b14f1aba9ca2573533d924afce5
Victor Briganti <jvbriganti@gmail.com>
2024-03-18 16:09:33 -0300

    [Views] Refatorado código
    Adicionado decoradores e padronizado respostas com o uso da
    rest_framework.

--- adopet_backend/usuario/urls.py
+++ adopet_backend/usuario/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from .views import usuario_list, usuario_detail
 
@@ -7,3 +8,5 @@ urlpatterns = [
     path("", usuario_list),
     path("<str:email>", usuario_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- adopet_backend/usuario/views.py
+++ adopet_backend/usuario/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Usuario
 from .serializers import UsuarioSerializer
@@ -9,49 +10,49 @@ from .serializers import UsuarioSerializer
 # Create your views here.
 
 
-def usuario_list(request):
+@api_view(["GET", "POST"])
+def usuario_list(request, format=None):
     """
     Lista todos os usuários ou cria um novo
     """
     if request.method == "GET":
         usuario = Usuario.objects.all()
         serializer = UsuarioSerializer(usuario, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = UsuarioSerializer(data=data)
+        serializer = UsuarioSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     return JsonResponse(serializer.errors, status=400)
 
 
-def usuario_detail(request, email):
+@api_view(["GET", "PUT", "DELETE"])
+def usuario_detail(request, email, format=None):
     """
     Retorna, atualiza ou deleta um usuário
     """
     try:
         usuario = Usuario.objects.get(email=email)
     except Usuario.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = UsuarioSerializer(usuario)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     if request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = UsuarioSerializer(usuario, data=data)
+        serializer = UsuarioSerializer(usuario, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         usuario.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
-    return JsonResponse(serializer.errors, status=400)
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
