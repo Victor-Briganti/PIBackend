e7ba190b1f93f0d2b85c172e20647c2920a0d3be
Victor Briganti <jvbriganti@gmail.com>
2024-03-18 00:34:31 -0300

    Removido código antigo

--- adopetBackend/usuario/admin.py
+++ adopetBackend/usuario/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
-from .models import Usuario, Endereco
-
 # Register your models here.
-
-admin.site.register(Usuario)
-admin.site.register(Endereco)
--- adopetBackend/usuario/urls.py
+++ adopetBackend/usuario/urls.py
@@ -1,11 +1,1 @@
 from django.urls import path
-
-from .views import UsuarioAPIView, EnderecoAPIView
-
-urlpatterns = [
-    path("usuario/", UsuarioAPIView.as_view()),
-    path("endereco/", EnderecoAPIView.as_view()),
-    path("usuario/<str:cpf>", UsuarioAPIView.as_view()),
-    path("endereco/<int:endereco_id>", EnderecoAPIView.as_view()),
-
-]
