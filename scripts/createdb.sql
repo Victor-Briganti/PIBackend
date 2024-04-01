c8ce2d09b6c556c85b9e9e18dfc34648f3f0f35d
Victor Briganti <jvbriganti@gmail.com>
2024-04-01 18:44:32 -0300

    [Database] Adicionado scripts de criação e deleção

---
+++ scripts/createdb.sql
@@ -0,0 +1,17 @@
+-- Cria o banco de dados
+CREATE DATABASE adopetbd;
+
+-- Cria o usuário
+CREATE USER adm_adopet WITH PASSWORD 'django8';
+
+-- Configurações necessárias para o funcionamento do django
+ALTER ROLE adm_adopet SET client_encoding TO 'utf8';
+ALTER ROLE adm_adopet SET default_transaction_isolation TO 'read committed';
+ALTER ROLE adm_adopet SET timezone TO 'UTC';
+
+-- Dá privilégios aos usuários
+GRANT ALL PRIVILEGES ON DATABASE adopetbd TO adm_adopet;
+
+-- Versão 15 do postgresql
+-- Permite que o banco de dados seja alterado pelo administrador
+ALTER DATABASE adopetbd OWNER TO adm_adopet;
---
+++ scripts/delete.sql
@@ -0,0 +1,5 @@
+-- Deleta o banco de dados
+DROP DATABASE adopetbd;
+
+-- Deleta o usuário
+DROP USER adm_adopet;
