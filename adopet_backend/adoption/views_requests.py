0ea3636dc66e7a8f762a07401ae7b64ad39427b7
MattSaito <matheussaitofs@gmail.com>
2024-06-05 11:34:59 -0300

    [listagem e filters] listagem e filters
    agora e possivel listar todos usuários que querem adotar e qual o animal
    que eles querem adotar
    
    Ao aceitar uma  adoção, todas as outras requisições do mesmo animal
    sao rejeitadas

---
+++ adopet_backend/adoption/views_requests.py
@@ -0,0 +1,116 @@
+from .models import Adoption
+from .serializers import AdoptionSerializer
+from animal.models import Animal
+from django.utils import timezone
+from rest_framework import status, permissions,filters
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from user.models import UserMetadata
+from animal.models import Animal
+from animal.serializers import AnimalSerializer
+
+class AdoptionRequestList(APIView):
+    """
+    Lista todas as solicitações de adoção 
+    """
+
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+    pagination_class = PageNumberPagination()
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ["request_status","comments"]
+    ordering_fields = ["request_date","response_date","request_status"]
+
+    def get(self, request):
+        user = request.user
+
+        search_filter = filters.SearchFilter()
+        ordering_filter = filters.OrderingFilter()
+
+
+        try:
+            _ = UserMetadata.objects.get(user=user)
+        except UserMetadata.DoesNotExist:
+            return Response(
+                "Usuário não foi propriamente cadastrado",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        adoption = Adoption.objects.all()
+        adoptions = search_filter.filter_queryset(request, adoption, self)
+        adoptions = ordering_filter.filter_queryset(request, adoptions, self)
+        serializer = AdoptionSerializer(adoptions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class AdoptionRequestDetailById(APIView):
+    """
+    Retorna a solicitação de adoção de acordo com o id da propria solicitacao
+    """
+
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+
+    def get(self, request, pk):
+        user = request.user
+
+        try:
+            _ = UserMetadata.objects.get(user=user)
+        except UserMetadata.DoesNotExist:
+            return Response(
+                "Usuário não foi propriamente cadastrado",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            adoption = Adoption.objects.get(pk=pk)
+        except Adoption.DoesNotExist:
+            return Response(
+                "Solicitação de adoção não encontrada", status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = AdoptionSerializer(adoption)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class AdoptionRequestAccept(APIView):
+    """
+    Aceita a solicitação de adoção e rejeita as outras solicitações para o mesmo animal
+    """
+
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+
+    def post(self, request, pk):
+        user = request.user
+
+        try:
+            _ = UserMetadata.objects.get(user=user)
+        except UserMetadata.DoesNotExist:
+            return Response(
+                "Usuário não foi propriamente cadastrado",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            adoption = Adoption.objects.get(pk=pk)
+        except Adoption.DoesNotExist:
+            return Response(
+                "Solicitação de adoção não encontrada", status=status.HTTP_404_NOT_FOUND
+            )
+
+        adoption.request_status = "approved"
+        adoption.animal.is_adopted = True
+        adoption.response_date = timezone.now()
+        adoption.save()
+        adoption.animal.save()
+        serializer = AdoptionSerializer(adoption)
+
+
+        remains = Adoption.objects.filter(animal=adoption.animal, request_status="pending")
+        for remain in remains:
+            remain.request_status = "rejected"
+            remain.response_date = timezone.now()
+            remain.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
